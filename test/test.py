b1125eb5557e02e08465cf605438bbd63cc6975d
fraoustin <fraoustin@gmail.com>
2019-08-13 17:28:00 +0200

    <feat> log: add SpecificLogLevel

--- test/test.py
+++ test/test.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, current_app
-from logging import DEBUG
+from logging import DEBUG, INFO
 
-from flask_easylog import EasyLog, FMT_ACCESS_LOG 
+from flask_easylog import EasyLog, FMT_ACCESS_LOG, log, SpecificLevelLog 
 
 app = Flask(__name__)
 app.secret_key = 'super secret string'
 app.logger.setLevel(DEBUG)
 
+app.logger.info("before add EasyLog")
+
 EasyLog(app, 
     fmt = FMT_ACCESS_LOG,
     afterlog = True,
     beforelog = True)
+
+app.logger.info("after add EasyLog")
 
 @app.route("/")
 def hello():
@@ -20,6 +24,18 @@ def hello():
     current_app.logger.debug("debug from hello")
     return "Hello World!"
 
+@app.route("/testone")
+@log
+def testone():
+    return "Test One"
+
+@log(DEBUG)
+@app.route("/testtwo")
+def testtwo():
+    return "Test Two"
+
+print(SpecificLevelLog()['tutu'])
+print(SpecificLevelLog()['testtwo'])
 
 if __name__ == "__main__":
     app.logger.info("after run")
